ccfccec78d909f58911246f4bd0034991bac59b4
Sergey Boyko <sergey.boyko.bse@gmail.com>
2020-09-14 07:45:08 -0400

    step3-7

---
+++ build/precache-manifest.08a778ea3075bac2a2cac7a2208f46f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0b2134b45555ffc274765e82a709864",
+    "url": "/index.html"
+  },
+  {
+    "revision": "576520d4687671128597",
+    "url": "/static/css/main.e5186507.chunk.css"
+  },
+  {
+    "revision": "5025582f0bba5a0d1c75",
+    "url": "/static/js/2.d0ffda05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d0ffda05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "576520d4687671128597",
+    "url": "/static/js/main.38433d73.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
---
+++ build/precache-manifest.c87c2e020ac0dc586533c1a0090c76b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1d96d90fa2e5df655ce9375decf3199",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83063daa79e4a727ba34",
+    "url": "/static/css/main.e5186507.chunk.css"
+  },
+  {
+    "revision": "5025582f0bba5a0d1c75",
+    "url": "/static/js/2.d0ffda05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d0ffda05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83063daa79e4a727ba34",
+    "url": "/static/js/main.e9dccc59.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
