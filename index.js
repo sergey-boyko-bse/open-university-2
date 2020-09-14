590ce3694e138d85bb5dbf9892afcf6183e66af3
Sergey Boyko <sergey.boyko.bse@gmail.com>
2020-09-14 11:58:16 -0400

    step3-11

--- index.js
+++ index.js
@@ -1,31 +1,15 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 
 app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-
-let persons = [
-    {
-      name: "Arto Hellas",
-      number: "040-123456",
-      id: 1
-    },
-    {
-      name: "Ada Lovelace",
-      number: "39-44-5323523",
-      id: 2
-    },
-    {
-      name: "Dan Abramov",
-      number: "999-000000",
-      id: 3
-    }
-]
 
 const generateId = () => {
     return Math.floor(Math.random() * 100000)
@@ -35,57 +19,59 @@ app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>')
 })
 
-app.get('/info', (req, res) => {
-    res.send(`<div>Phonebook has info for ${persons.length} people</div><div>${new Date()}</div>`)
-})
-
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+    Person.find({}).then(persons => {
+      res.json(persons)
+    })
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (req, res) => {
     const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    
-    if (person) {
-      response.json(person)
-    } else {
-      response.status(404).end()
-    }
+    Person.findById(id).then(person => {
+      if (person) {
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }
+    })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (req, res) => {
     const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-  
-    response.status(204).end()
+    Person.findByIdAndDelete(id).then(() => {
+      res.status(204).end()
+    })
 })
 
-app.post('/api/persons', (request, response) => {
-    const body = request.body
+app.post('/api/persons', (req, res) => {
+    const body = req.body
   
     if (!body || !body.name || !body.number) {
-        return response.status(400).json({ 
+        return res.status(400).json({ 
             error: 'name and number are required' 
         })
     }
 
-    const existingPerson = persons.find(x => x.name === body.name)
-    if (existingPerson) {
-        return response.status(400).json({ 
+    /*
+    Person.find({name: body.name}).then(existingPerson => {
+      if (existingPerson) {
+        return res.status(400).json({ 
             error: 'name must be unique' 
         })
-    }
-  
-    const person = {
-      name: body.name,
-      number: body.number,
-      id: generateId()
-    }
-  
-    persons = persons.concat(person)
-  
-    response.json(person)
+      }
+    */
+      const person = new Person({
+        name: body.name,
+        number: body.number,
+        id: generateId()
+      })
+    
+      person.save().then(savedPerson => {
+        res.json(savedPerson)
+      })
+    /*
+    })    
+    */
 })
 
 const PORT = process.env.PORT || 3001
